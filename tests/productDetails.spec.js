89c8e1e16ff36d05e84933d9ab71e9127239e891
Beatriz <beatriz.mediatto2@gmail.com>
2022-08-17 20:11:14 -0300

    oitavo requisito feito

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,14 +30,23 @@ const productDetails = require('../src/productDetails');
 */
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se productDetails é uma função.
-    // Teste se o retorno da função é um array.
-    // Teste se o array retornado pela função contém dois itens dentro.
-    // Teste se os dois itens dentro do array retornado pela função são objetos.
-    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
-    // Teste se os dois productIds terminam com 123.
-  });
+
+test('Teste se productDetails é uma função.', () => {
+  expect(typeof productDetails()).toBe('function');
+})
+test('Teste se o retorno da função é um array.', () => {
+  expect(productDetails())
+})
+test('Teste se o array retornado pela função contém dois itens dentro.', () => {
+
+})
+test('Teste se os dois itens dentro do array retornado pela função são objetos.', () => {
+
+})
+test('Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.', () => {
+
+})
+test('Teste se os dois productIds terminam com 123.', () => {
+
+})
 });
