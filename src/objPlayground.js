2a0961db3f7e87108f594fde6479ae42ce57c0d7
Beatriz <beatriz.mediatto2@gmail.com>
2022-08-18 07:37:44 -0300

    ajeitando erros de lint requisito 8

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -38,7 +38,7 @@ const calculator = (number1, number2) => {
   const cal = {
     sum: number1 + number2,
     mult: number1 * number2,
-    div: number1 / number2,
+    div: Math.floor(number1 / number2),
     sub: number1 - number2,
   };
   return cal;
@@ -48,10 +48,10 @@ const arrayGenerator = (type, object) => {
   if (type === 'keys') {
     return Object.keys(object);
   }
-  else if (type === 'values') {
+  if (type === 'values') {
     return Object.values(object);
   } 
-  else if (type === 'entries') {
+ if (type === 'entries') {
     return Object.entries(object);
   }
 };
